3083e6c56ba01604e2394f6154a51bc70acda607
Roman Podolets <romanpyu@gmail.com>
2023-10-04 13:09:49 +0300

    Add profile and comment models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.urls import reverse
 from django.utils import timezone
 
@@ -10,6 +12,21 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(status='published')
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/profile/")
+
+    def __str__(self):
+        return str(self.user)
+
+    @receiver(post_save, sender=User)
+    def create_or_update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ("draft", "Draft"),
@@ -17,7 +34,7 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique_for_date="publish")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="posts")
     image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')
     content = RichTextUploadingField()
     publish = models.DateTimeField(default=timezone.now)
@@ -37,3 +54,27 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('core:post_detail', args=[self.slug])
+
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="comments")
+    email = models.EmailField()
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return self.body
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
