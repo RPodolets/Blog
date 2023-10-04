ad5e31d6d5811ff91e65e7d2201274e5ccef4b33
Roman Podolets <romanpyu@gmail.com>
2023-10-04 11:41:13 +0300

    Add views for Post model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
+from core.models import Post
+
+
+class PostListView(generic.ListView):
+    model = Post
+    queryset = Post.published.all()
+    paginate_by = 3
+
+
+def post_detail(request: HttpRequest, post: str) -> HttpResponse:
+    post = get_object_or_404(Post, slug=post, status="published")
+    return render(request, "core/post_detail.html", {"post": post})
+
+# class PostDetailView(generic.DetailView):
+#     model = Post
+
