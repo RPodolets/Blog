d56e8bda719729414cc08ba7295e1c2f433a0b50
Roman Podolets <romanpyu@gmail.com>
2023-10-04 12:01:52 +0300

    Add ckeditor

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
@@ -17,7 +18,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    content = models.TextField()
+    content=RichTextUploadingField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
