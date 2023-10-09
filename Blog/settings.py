17e720b61947086b25751f3d7e462a9cd208215c
Roman Podolets <romanpyu@gmail.com>
2023-10-09 20:30:24 +0300

    Fixed db connect

--- Blog/settings.py
+++ Blog/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
